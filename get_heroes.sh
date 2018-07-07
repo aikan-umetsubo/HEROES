6d117456aecad07438d9c919d1a68aa27550b40f
aikan-umetsubo <yakinori21@gmail.com>
2018-07-07 12:36:24 +0900

    今日の開成山特別勝利を願ってオジュウチョウサン追加

--- get_heroes.sh
+++ get_heroes.sh
@@ -3,8 +3,8 @@
 # ヒーロー列伝のポスター画像フォルダのURL
 BASEURL='http://www.jra.go.jp/gallery/ads/heroes/popup/images/'
 
-# ヒーローの数（2018/01/01現在、ハイセイコーからキタサンブラックまで81頭）
-MAX=81
+# ヒーローの数（2018/07/01現在、ハイセイコーからオジュウチョウサンまで82頭）
+MAX=82
 
 # seqで連番を生成してループ、curlで画像を取得
 for NUM in `seq -f %003g 1 $MAX`
