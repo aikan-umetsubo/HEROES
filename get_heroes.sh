a5602409e178442d01377751fc4e5122a30200b8
aikan-umetsubo <yakinori21@gmail.com>
2019-01-15 17:47:50 +0900

    アーモンドアイ対応＆出力メッセージがcurlそのままだったのを変更

--- get_heroes.sh
+++ get_heroes.sh
@@ -3,12 +3,13 @@
 # ヒーロー列伝のポスター画像フォルダのURL
 BASEURL='http://www.jra.go.jp/gallery/ads/heroes/popup/images/'
 
-# ヒーローの数（2018/07/01現在、ハイセイコーからオジュウチョウサンまで82頭）
-MAX=82
+# ヒーローの数（2019/01/15現在、ハイセイコーからアーモンドアイまで83頭）
+MAX=83
 
 # seqで連番を生成してループ、curlで画像を取得
 for NUM in `seq -f %003g 1 $MAX`
 do
-    curl ${BASEURL}${NUM}m.jpg > ${NUM}m.jpg
+    curl --silent ${BASEURL}${NUM}m.jpg > ${NUM}m.jpg
+    echo ${NUM}m.jpg: Download complete.
 done
 
