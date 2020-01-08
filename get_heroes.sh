bbcde52fdfdc06bd1bd219f9f14c17af98f4b2de
aikan-umetsubo <yakinori21@gmail.com>
2020-01-08 23:51:07 +0900

    Modify: リスグラシューの圧倒的な有馬記念勝利を祝ってコミット

--- get_heroes.sh
+++ get_heroes.sh
@@ -3,8 +3,8 @@
 # ヒーロー列伝のポスター画像フォルダのURL
 BASEURL='http://www.jra.go.jp/gallery/ads/heroes/popup/images/'
 
-# ヒーローの数（2019/01/15現在、ハイセイコーからアーモンドアイまで83頭）
-MAX=83
+# ヒーローの数（2020/01/09現在、ハイセイコーからリスグラシューまで84頭）
+MAX=84
 
 # seqで連番を生成してループ、curlで画像を取得
 for NUM in `seq -f %003g 1 $MAX`
