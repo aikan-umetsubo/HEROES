8debeaa823a30167af57577ea52174e825f4cc67
aikan-umetsubo <yakinori21@gmail.com>
2024-10-28 00:40:26 +0900

    ドウデュースが天皇賞・秋を勝った日に更新

--- get_heroes.sh
+++ get_heroes.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # ヒーロー列伝のポスター画像フォルダのURL
-BASEURL='http://www.jra.go.jp/gallery/ads/heroes/popup/images/'
+BASEURL='https://jra.jp/gallery/ads/heroes/popup/images/'
 
-# ヒーローの数（2022/05/15現在、ハイセイコーからリスグラシューまで91頭）
-MAX=91
+# ヒーローの数（2024/10/27現在、ハイセイコーからドウデュースまで97頭）
+MAX=97
 
 # seqで連番を生成してループ、curlで画像を取得
 for NUM in `seq -f %003g 1 $MAX`
